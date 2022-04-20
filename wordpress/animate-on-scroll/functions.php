bd00c057692955218a6dbe6ea83332d53fd5ccf2
chynnalew <chynnalew@yahoo.com>
2022-04-20 12:54:15 -0700

    update animate with wow wordpress php functions

--- wordpress/animate-on-scroll/functions.php
+++ wordpress/animate-on-scroll/functions.php
@@ -6,18 +6,13 @@
  * Only edit this file if you have direct access to it on your server (to fix errors if they happen).
  */
 
-function generatepress_child_enqueue_scripts() {
-	if ( is_rtl() ) {
-		wp_enqueue_style( 'generatepress-rtl', trailingslashit( get_template_directory_uri() ) . 'rtl.css' );
-	}
-}
-add_action( 'wp_enqueue_scripts', 'generatepress_child_enqueue_scripts', 100 );
+//* Enqueue Custom.js Animate.CSS and WOW.js 
+add_action( 'wp_enqueue_scripts', 'gp_enqueue_scripts' );
 
-add_action( 'wp_enqueue_scripts', 'sk_enqueue_scripts' );
-
-function sk_enqueue_scripts() {
+function gp_enqueue_scripts() {
   wp_enqueue_style('animate', '//cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css');
-  wp_enqueue_style('wow-css', get_stylesheet_directory_uri() . '/css/animate.css', array(), true );
-  wp_enqueue_script('wow', get_stylesheet_directory_uri() . '/js/wow.min.js', array(), true );
-  wp_enqueue_script('custom-js-scripts', get_stylesheet_directory_uri() . '/js/custom.js', array(), true );
-}
+  wp_enqueue_style('wow-css', get_stylesheet_directory_uri() . '/css/animate.css', array(), '1.0', false );
+  wp_enqueue_script('wow', get_stylesheet_directory_uri() . '/js/wow.min.js', array(), '1.0', false);
+	wp_enqueue_script('custom', get_stylesheet_directory_uri() . '/js/custom.js', array(), '', true );
+  
+};
