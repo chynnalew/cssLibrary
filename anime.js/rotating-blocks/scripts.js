0f3014b4e53f8c70b0a8048da1d482d706cb9c3d
chynnalew <chynnalew@yahoo.com>
2022-05-16 10:13:20 -0700

    add delay function

--- anime.js/rotating-blocks/scripts.js
+++ anime.js/rotating-blocks/scripts.js
@@ -1,5 +1,5 @@
 anime({
-  targets: 'div.box.red',
+  targets: 'div.box',
   translateY: [
     { value: 200, duration: 500 },
     {value: 0, duration: 800}
@@ -7,44 +7,9 @@ anime({
   rotate: {
     value: '1turn',
     easing: 'easeInOutSine'
+  },
+  delay: function(element, iteration, length) {
+    return iteration * 1000
   }
 })
 
-anime({
-  targets: 'div.box.blue',
-  translateY: [
-    { value: 200, duration: 500, delay: '1000' },
-    {value: 0, duration: 800}
-  ],
-  rotate: {
-    value: '1turn',
-    easing: 'easeInOutSine',
-    delay: '1000'
-  }
-})
-
-anime({
-  targets: 'div.box.green',
-  translateY: [
-    { value: 200, duration: 500, delay: '2000' },
-    {value: 0, duration: 800}
-  ],
-  rotate: {
-    value: '1turn',
-    easing: 'easeInOutSine',
-    delay: '2000'
-  }
-})
-
-anime({
-  targets: 'div.box.yellow',
-  translateY: [
-    { value: 200, duration: 500, delay: '3000' },
-    {value: 0, duration: 800}
-  ],
-  rotate: {
-    value: '1turn',
-    easing: 'easeInOutSine',
-    delay: '3000'
-  }
-})
