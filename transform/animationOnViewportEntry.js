799b3209f892477736303a6a90dee1186a10c7bd
chynnalew <chynnalew@yahoo.com>
2023-08-15 10:00:27 -0700

    refactor animation on viewport to use better naming conventions

--- transform/animationOnViewportEntry.js
+++ transform/animationOnViewportEntry.js
@@ -1,10 +1,10 @@
 //alter code based on your keyframe animation name and element class
 // works for all elements with the same class, triggering animation as they come onto the screen
 
-//EXAMPLE: wipe in animation from the right
+//EXAMPLE: wipe in animation from the left
 
 //css
-@keyframes wipe-in-right {
+@keyframes wipe-in-left {
     from {
         clip-path: inset(0 100% 0 0);
     }
@@ -12,21 +12,21 @@
         clip-path: inset(0 0 0 0);
     }
 }
-[transition-style="in:wipe:right"] {
-    animation: 3s cubic-bezier(.25, 1, .30, 1) wipe-in-right both;
+[transition-style="in:wipe:left"] {
+    animation: 3s cubic-bezier(.25, 1, .30, 1) wipe-in-left both;
 }
 
 //js
-//Add wipe in from right animation
+//Add wipe in from left animation
 // alter threshold number to trigger based on percent of element in viewport scale of 0 - 1
-const wipeRightElements = document.querySelectorAll('.wipe-right');
-const wipeRightObserver = new IntersectionObserver(entries => {
+const wipeLeftElements = document.querySelectorAll('.wipe-left');
+const wipeLeftObserver = new IntersectionObserver(entries => {
     entries.forEach(entry => {
-        entry.target.classList.toggle('wipe-r', entry.isIntersecting);
-        $('.wipe-r').attr('transition-style', 'in:wipe:right');
+        entry.target.classList.toggle('wipe-l', entry.isIntersecting);
+        $('.wipe-l').attr('transition-style', 'in:wipe:left');
         console.log(entry);
     }, { threshold: .3 });
 });
-wipeRightElements.forEach(wipeRightElement => {
-    wipeRightObserver.observe(wipeRightElement);
+wipeLeftElements.forEach(wipeLeftElement => {
+    wipeLeftObserver.observe(wipeLeftElement);
 })
